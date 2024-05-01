939fcc766163d403191148c75c9ed9e6fcf9387b
akashyapb <158523269+akashyapb@users.noreply.github.com>
2024-05-01 16:12:57 -0500

    Changes to org and both models complete commit
    There is some re organisation to the repository and completed commits of Model 1 and Model 2. **Time complexity to be still considered

--- Projects/Final Project/Code/Model2.py
+++ Projects/Final Project/Code/Model2.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split, cross_val_score
-from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
 import warnings
 
@@ -70,7 +70,7 @@ y = merged_data.drop(columns=['year', 'month', 'day', 'Best Station ID', 'h1', '
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Step 6: Model Training
-model = LogisticRegression()
+model = RandomForestRegressor()
 model.fit(X_train, y_train)
 
 # Step 7: Model Evaluation
